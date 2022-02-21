f1fbd46f15c10fcff29d2ce336f97b7c1fbcacfc
Ressigui <ridouane.essigui@gmail.com>
2022-02-21 10:26:34 +0100

    Amazon KDP

---
+++ README.md
@@ -0,0 +1,2 @@
+Modifier .env_lock to .env 
+inserer your mail et votre mot de passe
--- index.js
+++ index.js
@@ -25,14 +25,51 @@ console.log(data)
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({headless:false});
+    
+  const browser = await puppeteer.launch({ headless: false,defaultViewport: null });
   const page = await browser.newPage();
   await page.goto('https://kdp.amazon.com/fr_FR/title-setup/paperback/new/details?ref_=kdp_kdp_BS_D_cr_ti', {
     waitUntil: 'networkidle2',
   });
- let bodyHTML=await page.evaluate(()=>document.body.innerHTML)
-  console.log(bodyHTML)
 
-  // await browser.close();
+  await page.type('[name=email]',process.env.EMAIL,{delay:200})
+  await page.type('[name=password]',process.env.PASSWORD,{delay:200})
+  //await page.click("input[type=checkbox]");
+  await page.click("input[type=submit]");
+  await page.waitForSelector("#data-print-book-title",{visible:true});
+  await page.click("#data-print-book-title");
+  await page.type('#data-print-book-title', "Hi I'M TITLE2", {delay: 120,});
+  await page.type('#data-print-book-subtitle', "Hi I'M TITLE", {delay: 120,});
+  await page.type("#data-print-book-primary-author-first-name","Hi I'M A FIRST NAME",{ delay: 120 });
+  await page.type("#data-print-book-primary-author-last-name","Hi I'M A FIRST NAME",{ delay: 120 });
+
+
+
+
+  await page.type("#data-print-book-description", "Hi I'M A DESCRIPTION", {delay: 120});
+  await page.click("#non-public-domain", {delay: 25,});
+  await page.type("#data-print-book-keywords-0", "HY I'M A SUBTITLE 1", {delay: randomInt(150, 250),});
+  await page.type("#data-print-book-keywords-1", "HY I'M A SUBTITLE 2", {delay: randomInt(150, 250),});
+  await page.type("#data-print-book-keywords-2", "HY I'M A SUBTITLE 3", {delay: randomInt(150, 250),});
+  await page.type("#data-print-book-keywords-3", "HY I'M A SUBTITLE 4", {delay: randomInt(150, 250),});
+  await page.type("#data-print-book-keywords-4", "HY I'M A SUBTITLE 5", {delay: randomInt(150, 250),});
+  await page.type("#data-print-book-keywords-5", "HY I'M A SUBTITLE 6", {delay: randomInt(150, 250),});
+  await page.type("#data-print-book-keywords-6", "HY I'M A SUBTITLE 7", {delay: randomInt(150, 250),});
+  await page.click("#data-print-book-categories-button-proto-announce");
+  await page.$$eval("#category-chooser-popover .a-link-normal", (links) =>links.forEach((link) => link.click()));
+  await page.click("#data-print-book-categories-button-proto-announce");
+  await page.click("#unsaved-changes-cancel-announce");
+
+  await page.$$eval(".a-label", (links) =>
+  links.forEach((el) => {
+    el.textContent === "Global Warming & Climate Change" ? el.click() : true;
+    el.textContent === "Meteorology & Climatology" ? el.click() : true;
+  })
+);
+  await page.$$eval(".a-button-input", (elements) => elements[4].click());
+  await page.$$eval(".jele-override-input-width-radio input", (elements) =>elements[0].click());
+  await page.click("#save-and-continue-announce");
+
+  // await browser.close(); data[print_book][subtitle]
 })();
  
